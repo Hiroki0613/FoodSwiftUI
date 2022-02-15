32676a52d8db7aa349227a4fad12ddf2b80cd733
Hiroki Kondo <kondo.matr02@gmail.com>
2022-02-15 22:16:55 +0900

    Initial Commit

---
+++ FoodSwiftUI/FoodSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodSwiftUIApp.swift
+//  FoodSwiftUI
+//
+//  Created by 近藤宏輝 on 2022/02/15.
+//
+
+import SwiftUI
+
+@main
+struct FoodSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
