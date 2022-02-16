06366471ac83c2c201c3081e62cef382ba970468
Hiroki Kondo <kondo.matr02@gmail.com>
2022-02-16 18:00:49 +0900

    AppetizerDetailViewとのAPI繋ぎ込みを完了

--- FoodSwiftUI/Screens/AppetizerListView/AppetizerListView.swift
+++ FoodSwiftUI/Screens/AppetizerListView/AppetizerListView.swift
@@ -13,28 +13,42 @@ struct AppetizerListView: View {
     
     @StateObject var viewModel = AppetizerListViewModel()
     
+    @State private var isShowingDetail = false
+    @State private var selectedAppetizer: Appetizer?
     
     var body: some View {
         ZStack {
             NavigationView {
                 List(viewModel.appetizers) { appetizer in
                     AppetizerListCell(appetizer: appetizer)
+                        .onTapGesture {
+                            selectedAppetizer = appetizer
+                            isShowingDetail = true
+                        }
                 }
                 .navigationTitle("🍟 Appetizers")
+                .disabled(isShowingDetail)
             }
             .onAppear {
                 viewModel.getAppetizers()
             }
-            .alert(item: $viewModel.alertItem) { alertItem in
-                Alert(
-                    title: alertItem.title,
-                    message: alertItem.message,
-                    dismissButton: alertItem.dismissButton)
+            
+            .blur(radius: isShowingDetail ? 20 : 0)
+            
+            if isShowingDetail {
+                AppetizerDetailView(appetizer: selectedAppetizer!,
+                                    isShowingDetail: $isShowingDetail)
             }
             
             if viewModel.isLoading {
                 LoadingView()
             }
+        }
+        .alert(item: $viewModel.alertItem) { alertItem in
+            Alert(
+                title: alertItem.title,
+                message: alertItem.message,
+                dismissButton: alertItem.dismissButton)
         }
     }
 }
